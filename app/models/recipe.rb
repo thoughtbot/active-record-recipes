7018cd3f0a0517a0a9b0bc739b90675b85212eb0
Steve Polito <stevepolito@hey.com>
2022-03-20 07:46:33 -0400

    Group Chefs by number of Recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,4 +11,8 @@ class Recipe < ApplicationRecord
     select("DISTINCT ON(recipes.chef_id) recipes.*")
       .order(:chef_id, created_at: :desc)
   }
+
+  scope :per_chef, -> {
+    group(:chef_id).count
+  }
 end
